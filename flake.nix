d2e8e4de2cbe6beff93f43e4d31f7cc2f3c3c006
Michael Koeppl <michael@koeppl.dev>
2025-12-22 20:12:31 +0100

    add Nix dev shell
    Signed-off-by: Michael Koeppl <michael@koeppl.dev>

--- flake.nix
+++ flake.nix
@@ -1,33 +1,17 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, fenix, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        devToolchain = fenix.packages."${system}".stable;
       in rec {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
-            (devToolchain.withComponents [
-              "cargo"
-              "rustc"
-              "rust-src"
-              "rustfmt"
-              "clippy"
-              "rust-analyzer"
-            ])
+            pkgs.rustup
           ];
-          shellHook = ''
-            export PATH=$PATH:~/.cargo/bin
-          '';
-          RUST_BACKTRACE = 1;
         };
       });
 }
