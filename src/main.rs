e2543a9edbd48780291ee26c35e232d2ba4541b9
Michael Koeppl <michael@koeppl.dev>
2025-12-23 22:07:36 +0100

    add initial unit tests
    Signed-off-by: Michael Koeppl <michael@koeppl.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod qemu;
+mod serial;
 mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    let x = [1, 2, 3];
-    println!("Value: {}", x[get_bad_index()]);
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -19,8 +24,53 @@ fn get_bad_index() -> usize {
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     print!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    use crate::qemu::exit_qemu;
+
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", _info);
+    exit_qemu(qemu::QemuExitCode::Failed);
+
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    use crate::qemu::exit_qemu;
+
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(qemu::QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
